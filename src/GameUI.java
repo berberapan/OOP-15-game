bc4807a1e4d1b1d7e16e789fe5c1f5f629888aa4
Tuan <tarreng@gmail.com>
2023-10-29 00:25:12 +0200

    Commit first layout2

--- src/GameUI.java
+++ src/GameUI.java
@@ -2,32 +2,32 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameUI extends JFrame {
-    JPanel platform = new JPanel();
+    BorderLayout layout = new BorderLayout();
+    JPanel platform = new JPanel(layout);
     JPanel northPanel = new JPanel();
     JPanel southPanel = new JPanel();
 
     JLabel gameStatusLabel = new JLabel("Game Status: Game on");
 
     JButton newGameButton = new JButton("New Game");
-    JButton button1 = new JButton("1");
-    JButton button2 = new JButton("2");
-    JButton button3 = new JButton("3");
-    JButton button4 = new JButton("4");
+
+    JButton numberButton = new JButton();
 
     GameUI() {
-        add(platform);
-
-        platform.add(northPanel, BorderLayout.NORTH);
-        platform.add(southPanel, BorderLayout.SOUTH);
-
         northPanel.add(gameStatusLabel);
         northPanel.add(newGameButton);
 
         southPanel.setLayout(new GridLayout(4,4));
-        southPanel.add(button1);
-        southPanel.add(button2);
-        southPanel.add(button3);
-        southPanel.add(button4);
+        for (int i = 1; i <= 15; i++) {
+            numberButton = new JButton(Integer.toString(i));
+            numberButton.setPreferredSize(new Dimension(70, 70));
+            southPanel.add(numberButton);
+        }
+
+        platform.add(northPanel, BorderLayout.NORTH);
+        platform.add(southPanel, BorderLayout.SOUTH);
+
+        add(platform);
 
         pack();
         setLocationRelativeTo(null);
